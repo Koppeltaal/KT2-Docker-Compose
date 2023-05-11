01e0f82f148dffe9613fe7d1a8979a8865678bf1
Joris Scharp <joris@headease.nl>
2023-05-11 20:38:53 +0200

    KLS-1: Replaces nginx and IRMA with Keycloak. The docker compose automatically imports the kt2 realm. Can be used to login to domain-admin now, the "user registration" is enabled in the realm, any registered users have full access.

--- sql/create_databases.sql
+++ sql/create_databases.sql
@@ -4,3 +4,10 @@ CREATE DATABASE domain_admin_dev
     ENCODING = 'UTF8'
     CONNECTION LIMIT = -1
     IS_TEMPLATE = False;
+
+CREATE DATABASE keycloak
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
